e2abdd62c3c50ae9572bf5254106bf7d318cd2d4
April <simaqingshan666@gmail.com>
2023-03-06 18:18:20 +0800

    finish all 3.6 18:17 version1

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,72 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class TestArrayDequeGold {
+    @Test
+    public void testaddFirst() {
+        ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
+        for (int i = 2; i < 10000; i++) {
+            int number = StdRandom.uniform(100);
+            a.addFirst(number);
+            b.addFirst(number);
+            int index = StdRandom.uniform(i-1);
+            assertEquals(a.get(index), b.get(index));
+        }
+    }
+
+    @Test
+    public void testaddLast() {
+        ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
+        for (int i = 2; i < 10000; i++) {
+            int number = StdRandom.uniform(100);
+            a.addLast(number);
+            b.addLast(number);
+            int index = StdRandom.uniform(i-1);
+            assertEquals(a.get(index), b.get(index));
+        }
+    }
+
+    @Test
+    public void testremoveFirst() {
+        ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
+        String message = "";
+        for (int i = 0; i < 20; i++) {
+            int number = StdRandom.uniform(10);
+            a.addLast(number);
+            b.addLast(number);
+            message += "AddLast(" + number + ")\n";
+        }
+        for (int i = 1; i< 20; i++) {
+            Integer x = a.removeFirst();
+            Integer y = b.removeFirst();
+            message += "removeFirst()\n";
+            assertEquals(message, x, y);
+            //int index = StdRandom.uniform(a.size());
+            //assertEquals(a.get(index), b.get(index));
+        }
+    }
+
+    @Test
+    public void testremoveLast() {
+        ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
+        String message = "";
+        for (int i = 0; i < 10; i++) {
+            int number = StdRandom.uniform(10);
+            a.addLast(number);
+            b.addLast(number);
+           message += "AddLast(" + number + ")\n";
+        }
+        for (int i = 1; i< 10; i++) {
+            Integer x = a.removeLast();
+            Integer y = b.removeLast();
+            message += "removeLast()\n";
+            assertEquals(message, x, y);
+            //int index = StdRandom.uniform(a.size());
+            //assertEquals(a.get(index), b.get(index));
+        }
+
+    }
+}
