e086bb5ae0ca670a3fbe3b49b65a29c07bd673a0
April <simaqingshan666@gmail.com>
2023-03-08 07:30:47 +0800

    3.8 7:30 modify proj1gold 4tests->1test

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,41 +1,32 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 public class TestArrayDequeGold {
-    public ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
-    public StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
     @Test
-    public void testaddFirst() {
-        for (int i = 2; i < 10000; i++) {
+    public void teststudent() {
+        ArrayDequeSolution<Integer> a = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> b = new StudentArrayDeque<>();
+        String message = "";
+        //testaddFirst()
+        for (int i = 2; i < 10; i++) {
             int number = StdRandom.uniform(100);
             a.addFirst(number);
             b.addFirst(number);
+            message += "AddLast(" + number + ")\n";
             int index = StdRandom.uniform(i - 1);
             assertEquals(a.get(index), b.get(index));
         }
-    }
-
-    @Test
-    public void testaddLast() {
-        String message = "";
-        for (int i = 2; i < 10000; i++) {
+        //testaddLast() {
+        for (int i = 2; i < 10; i++) {
             int number = StdRandom.uniform(100);
             a.addLast(number);
             b.addLast(number);
+            message += "AddLast(" + number + ")\n";
             int index = StdRandom.uniform(i - 1);
             assertEquals(a.get(index), b.get(index));
         }
-    }
 
-    @Test
-    public void testremoveFirst() {
-        String message = "";
-        for (int i = 0; i < 20; i++) {
-            int number = StdRandom.uniform(10);
-            a.addLast(number);
-            b.addLast(number);
-            message += "AddLast(" + number + ")\n";
-        }
-        for (int i = 1; i < 20; i++) {
+        //testremoveFirst() {
+        for (int i = 1; i < 10; i++) {
             Integer x = a.removeFirst();
             Integer y = b.removeFirst();
             message += "removeFirst()\n";
@@ -43,17 +34,8 @@ public class TestArrayDequeGold {
             //int index = StdRandom.uniform(a.size());
             //assertEquals(a.get(index), b.get(index));
         }
-    }
 
-    @Test
-    public void testremoveLast() {
-        String message = "";
-        for (int i = 0; i < 10; i++) {
-            int number = StdRandom.uniform(10);
-            a.addLast(number);
-            b.addLast(number);
-            message += "AddLast(" + number + ")\n";
-        }
+        //testremoveLast() {
         for (int i = 1; i < 10; i++) {
             Integer x = a.removeLast();
             Integer y = b.removeLast();
