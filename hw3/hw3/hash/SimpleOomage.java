6082aa9c516c3119664d83dd8bbb488e1d02aef3
April <simaqingshan666@gmail.com>
2023-03-16 14:24:14 +0800

    0316 14:24 finish hw2

--- hw3/hw3/hash/SimpleOomage.java
+++ hw3/hw3/hash/SimpleOomage.java
@@ -10,26 +10,30 @@ public class SimpleOomage implements Oomage {
     protected int blue;
 
     private static final double WIDTH = 0.01;
-    private static final boolean USE_PERFECT_HASH = false;
+    private static final boolean USE_PERFECT_HASH = true;
 
     @Override
     public boolean equals(Object o) {
-        // TODO: Write this method.
+        if (o == null) {
+            return false;
+        }
+        if (o.getClass() == SimpleOomage.class) {
+            SimpleOomage x = (SimpleOomage) o;
+            if (this.red == x.red && this.blue == x.blue &&this.green == x.green) {
+                return true;
+            }
+        }
         return false;
     }
 
-    /* Uncomment this method after you've written
-       equals and failed the testHashCodeAndEqualsConsistency
-       test.
     @Override
     public int hashCode() {
         if (!USE_PERFECT_HASH) {
             return red + green + blue;
         } else {
-            // TODO: Write a perfect hash function for Simple Oomages.
-            return 0;
+            return 1000000 * red + 1000 * green + red ;
         }
-    }*/
+    }
 
     public SimpleOomage(int r, int g, int b) {
         if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
