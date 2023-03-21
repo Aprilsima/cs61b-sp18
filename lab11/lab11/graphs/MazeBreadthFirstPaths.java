cda2ebb455662d9c1d5b7c93c9db78f8fbcb199f
April <simaqingshan666@gmail.com>
2023-03-21 17:13:56 +0800

    0321 17:13 finish lab11 first two part

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +12,60 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+    private Queue<Integer> queue;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        queue = new ArrayDeque<>();     //so shocked! "Queue is an interface. You can't instantiate an interface
+                                        //directly except via an anonymous inner class. Typically this isn't what
+                                        //you want to do for a collection. Instead, choose an existing implementation."
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int v) {
+        marked[v] = true;
+        if (marked[t] == true) {
+            targetFound = true;
+        }
+        if (targetFound) {
+            announce();
+            return;
+        }
+
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                queue.add(w);
+                edgeTo[w] = v;
+                announce();
+                distTo[w] = distTo[v] + 1;
+                if (targetFound) {
+                    return;
+                }
+            }
+        }
+        int size = queue.size();
+        for (int i = 0; i < size; i++) {
+            if (targetFound) {
+                announce();
+                return;
+            }
+            bfs(queue.poll());
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,55 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int[] parent;
+    private boolean hascycle;
 
     public MazeCycles(Maze m) {
         super(m);
+        hascycle = false;
+        parent = new int[maze.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cycle(0);
     }
 
-    // Helper methods go here
+    public void cycle(int v) {
+        marked[v] = true;
+        announce();
+        if (hascycle) {
+            return;
+        }
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                parent[w] = v;
+                cycle(w);
+                if (hascycle) {
+                    return;
+                }
+            } else {
+                if (parent[v] != w) {
+                    hascycle = true;
+                    parent[w] = v;
+                    drawparent(w, v);
+                    edgeTo[w] = v;
+                    announce();
+                    return;
+                }
+            }
+        }
+
+    }
+
+    public void drawparent(int source, int target) {
+        if (source == target) {
+            return;
+        }
+        drawparent(source, parent[target]);
+        edgeTo[target] = parent[target];
+        announce();
+    }
+
 }
 
